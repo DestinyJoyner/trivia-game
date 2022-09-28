481ef8d7d6c3a62e82c6313fe600340d593d06f5
DestinyJoyner <destinyjoyner@pursuit.org>
2022-09-28 16:17:26 -0400

    import export for functions from another js file to main js file success

---
+++ functions.js
@@ -0,0 +1,99 @@
+
+export let updatedResults;
+export let index = 0
+
+export const question = document.querySelector('#question')
+export const answers = document.querySelector(`#answers`)
+export const newGame = document.querySelector(`#newGame`)
+export const scoreSpan = document.querySelector(`.score`)
+export let score = 0
+
+// Put for loop in function to repopulate answer options
+const updateQuestion = () => {
+    question.innerHTML = updatedResults[index].question
+    if(document.querySelector(`#newGame`)){
+        document.querySelector(`#newGame`).style.visibility = `hidden`
+    }
+
+    for (let i = 0; i < 4; i++){
+        const div = document.createElement(`div`)
+        const randomNum = Math.floor(Math.random() * updatedResults[index].incorrect_answers.length) 
+
+        div.innerHTML = updatedResults[index].incorrect_answers.splice(randomNum, 1)
+        answers.append(div)
+    }
+}
+
+const fetchInfo = async () => {
+    const resp = await fetch(`https://opentdb.com/api.php?amount=3&type=multiple`)
+    const respJson = await resp.json()
+
+    updatedResults = respJson.results.map((question) => {
+       question.incorrect_answers.push(question.correct_answer)
+       return question
+    })
+    updateQuestion()
+    
+}
+
+// function for display after correct or incorrect answer is selected
+const answerChoice = (choice) => {
+    answers.classList.toggle(`${choice}`)
+    if(choice === `correctAnswer`){
+        answers.innerHTML =  `Correct!`
+    }
+    else {
+        answers.innerHTML = `Incorrect!`
+    }
+    setTimeout(() =>{
+        answers.innerHTML = ``
+        answers.classList.remove(`${choice}`)
+        index++
+        if(index >= updatedResults.length){
+            question.innerHTML = `${scoreSpan.innerHTML}`
+            answers.innerHTML = ``
+            newGame.style.visibility = `visible`
+        }
+        else{
+            updateQuestion()
+        }
+        
+    }, 1000)
+    
+}
+
+
+//Event listener for correct answer choice
+answers.addEventListener(`click`, (event) => {
+    if(event.target.innerHTML === updatedResults[index].correct_answer){
+        score += 10
+        scoreSpan.innerHTML = score
+        answerChoice(`correctAnswer`)
+    }
+    // Eliminates turning background color of answers red when area around answer choices is clicked
+    else if(event.target.innerHTML !== answers.innerHTML && event.target.innerHTML !== updatedResults[index].correct_answer ) {
+        event.target.style.backgroundColor = `red`
+        const answerOptions = document.querySelectorAll(`#answers div`)
+        answerOptions.forEach(ans => {
+            if(ans.innerHTML === updatedResults[index].correct_answer){
+                ans.style.backgroundColor = `green`
+            }
+        })
+        setTimeout(() => {
+            answerChoice(`incorrectAnswer`)
+        }, 2000)
+    }
+})
+
+// Event listener for 'New Game Button`
+newGame.addEventListener(`click`, () => {
+    index = 0
+    fetchInfo()
+})
+
+
+export {
+    updateQuestion,
+    fetchInfo,
+    answerChoice,
+}
--- main.js
+++ main.js
@@ -1,89 +1,118 @@
 // await functions/ async fetch
 
-let updatedResults;
-let index = 0
+import {updatedResults, index, question, answers, newGame, scoreSpan, score, 
+    updateQuestion,
+    fetchInfo,
+    answerChoice,
+    } from './functions.js'
+// let updatedResults;
+// let index = 0
 
-const question = document.querySelector('#question')
-const answers = document.querySelector(`#answers`)
-const next = document.querySelector(`#next`)
-// console.log(question,answers,next)
-const scoreSpan = document.querySelector(`.score`)
-let score = 0
+// const question = document.querySelector('#question')
+// const answers = document.querySelector(`#answers`)
+// const newGame = document.querySelector(`#newGame`)
+// // console.log(question,answers,next)
+// const scoreSpan = document.querySelector(`.score`)
+// let score = 0
 
-// Put for loop in function to repopulate answer options
-const updateQuestion = () => {
-    question.innerHTML = updatedResults[index].question
+// // Put for loop in function to repopulate answer options
+// const updateQuestion = () => {
+//     console.log(question)
+//     question.innerHTML = updatedResults[index].question
+//     if(document.querySelector(`#newGame`)){
+//         document.querySelector(`#newGame`).style.visibility = `hidden`
+//     }
 
-    for (let i = 0; i < 4; i++){
-        const div = document.createElement(`div`)
-        const randomNum = Math.floor(Math.random() * updatedResults[index].incorrect_answers.length) 
+//     for (let i = 0; i < 4; i++){
+//         const div = document.createElement(`div`)
+//         const randomNum = Math.floor(Math.random() * updatedResults[index].incorrect_answers.length) 
 
-        div.innerHTML = updatedResults[index].incorrect_answers.splice(randomNum, 1)
-        answers.append(div)
-    }
-}
+//         div.innerHTML = updatedResults[index].incorrect_answers.splice(randomNum, 1)
+//         answers.append(div)
+//     }
+// }
 
-const fetchInfo = async () => {
-    const resp = await fetch(`https://opentdb.com/api.php?amount=10&type=multiple`)
-    const respJson = await resp.json()
-    // console.log(respJson)
+// const fetchInfo = async () => {
+//     const resp = await fetch(`https://opentdb.com/api.php?amount=3&type=multiple`)
+//     const respJson = await resp.json()
+//     // console.log(respJson)
 
-    updatedResults = respJson.results.map((question) => {
-       question.incorrect_answers.push(question.correct_answer)
-       return question
-    })
-    // console.log(updatedResults)
-    updateQuestion()
+//     updatedResults = respJson.results.map((question) => {
+//        question.incorrect_answers.push(question.correct_answer)
+//        return question
+//     })
+//     console.log(updatedResults)
+//     updateQuestion()
     
-}
+// }
 
-// function for display after correct or incorrect answer is selected
-const answerChoice = (choice) => {
-    answers.classList.toggle(`${choice}`)
-    if(choice === `correctAnswer`){
-        answers.innerHTML =  `Correct!`
-    }
-    else {
-        answers.innerHTML = `Incorrect!`
-    }
-    setTimeout(() =>{
-        answers.innerHTML = ``
-        answers.classList.remove(`${choice}`)
-        index++
-        updateQuestion()
-    }, 1000)
-}
+// // function for display after correct or incorrect answer is selected
+// const answerChoice = (choice) => {
+//     answers.classList.toggle(`${choice}`)
+//     if(choice === `correctAnswer`){
+//         answers.innerHTML =  `Correct!`
+//     }
+//     else {
+//         answers.innerHTML = `Incorrect!`
+//     }
+//     setTimeout(() =>{
+//         answers.innerHTML = ``
+//         answers.classList.remove(`${choice}`)
+//         index++
+//         if(index >= updatedResults.length){
+//             question.innerHTML = `${scoreSpan.innerHTML}`
+//             answers.innerHTML = ``
+//             newGame.style.visibility = `visible`
+//         }
+//         else{
+//             updateQuestion()
+//         }
+        
+//     }, 1000)
+    
+// }
 
 
-//Event listener for correct answer choice
-answers.addEventListener(`click`, (event) => {
-    if(event.target.innerHTML === updatedResults[index].correct_answer){
-        score += 10
-        scoreSpan.innerHTML = score
-        answerChoice(`correctAnswer`)
-       
-    }
-    else {
-        event.target.style.backgroundColor = `red`
-        const answerOptions = document.querySelectorAll(`#answers div`)
-        answerOptions.forEach(ans => {
-            if(ans.innerHTML === updatedResults[index].correct_answer){
-                ans.style.backgroundColor = `green`
-            }
-        })
-        setTimeout(() => {
-            answerChoice(`incorrectAnswer`)
-        }, 2000)
-         
-    }
-    //Make answers 'unclickable`
-    // answers.style.pointerEvents = `none`
-})
+// //Event listener for correct answer choice
+// const answerOptions = document.querySelectorAll(`.answerChoice`)
+// answers.addEventListener(`click`, (event) => {
+//     if(event.target.innerHTML === updatedResults[index].correct_answer){
+//         score += 10
+//         scoreSpan.innerHTML = score
+//         answerChoice(`correctAnswer`)
+//     }
+//     // Eliminates turning background color of answers red when area around answer choices is clicked
+//     else if(event.target.innerHTML !== answers.innerHTML && event.target.innerHTML !== updatedResults[index].correct_answer ) {
+//         event.target.style.backgroundColor = `red`
+//         const answerOptions = document.querySelectorAll(`#answers div`)
+//         answerOptions.forEach(ans => {
+//             if(ans.innerHTML === updatedResults[index].correct_answer){
+//                 ans.style.backgroundColor = `green`
+//             }
+//         })
+//         setTimeout(() => {
+//             answerChoice(`incorrectAnswer`)
+//         }, 2000)
+//     }
+// })
+
+// Event listener for 'New Game Button`
+// newGame.addEventListener(`click`, () => {
+//     index = 0
+//     fetchInfo()
+// })
+
+
+// No functions below this point, only what needs to be run on page load
 
 fetchInfo()
 
 
+
 // UNUSED FUNCTIONS
+
+ //Make answers 'unclickable`
+    // answers.style.pointerEvents = `none`
 
 //Event Listener for 'next' button to generate new question and answers
 // next.addEventListener(`click`, () => {
