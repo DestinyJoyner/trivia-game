3a2f08c2e93f78fe34eb313a268d7b790a10bba2
DestinyJoyner <destinyjoyner@pursuit.org>
2022-09-28 16:50:48 -0400

    changed prompt to input text on page

--- main.js
+++ main.js
@@ -15,11 +15,20 @@ import {updatedResults, index, question, answers, newGame, scoreSpan, score,
     } from './functions.js'
 
 // No functions below this point, only what needs to be run on page load
-let user = prompt(`Hello! Welcome to Trivia! Please Enter You're Name To Get Started`)
-if(user){
-    document.querySelector(`.user`).innerText = user
-    fetchInfo()
-}
+/* 
+<h1>Hey, <span class="user">(Name)</span> What do you know? Do You Know Things? Let's Find Out!</h1>
+*/
+// let user = prompt(`Hello! Welcome to Trivia! Please Enter You're Name To Get Started`)
+const playButton = document.querySelector(`#playButton`)
+const nameInput = document.querySelector(`#userName`)
+playButton.addEventListener(`click`, () => {
+    if(nameInput.value){
+        document.querySelector(`header`).innerHTML = `
+        <h1>Hey, <span class="user">${nameInput.value}</span> What do you know? Do You Know Things? Let's Find Out!</h1>`
+        fetchInfo()
+    }
+})
+
 
 
 
